5e6edfda65918b5534726b342a82dcf6b046749c
Daniel Kreck <daniel.kreck@mni.thm.de>
2014-11-29 00:10:44 +0100

    24. AutoSlide Entfernt

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -303,7 +303,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   Carousel.VERSION  = '3.2.0'
 
   Carousel.DEFAULTS = {
-    interval: 5000,
+    interval: false, /*Änderung des Standardwertes interval: 5000,*/
     pause: 'hover',
     wrap: true
   }
